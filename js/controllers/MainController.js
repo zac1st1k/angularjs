f62ec5870c4b61e690312cda381d95a57db2c675
zac1st1k <zacfirst@gmail.com>
2015-05-18 10:23:21 +1000

    + Rating Button

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,8 +1,13 @@
 var app = angular.module("myApp", []);
 app.controller('MainController', ['$scope', function($scope) {
   $scope.title = 'Top Sellers in Books!';
-  $scope.promo = 'FREE shipping! One week only!';
+  $scope.promo = 'Free Shipping!';
   $scope.products = 
-    [{ name: 'The Book of Trees', price: 19, pubdate: new Date('2014', '03', '08'), cover: 'img/the-book-of-trees.jpg' }, 
-    { name: 'Program or be Programmed', price: 8, pubdate: new Date('2013', '08', '01'), cover: 'img/program-or-be-programmed.jpg' }]
+    [{ name: 'The Book of Trees', price: 19, pubdate: new Date('2014', '03', '08'), cover: 'img/the-book-of-trees.jpg', likes: 0 }, 
+    { name: 'Program or be Programmed', price: 8, pubdate: new Date('2013', '08', '01'), cover: 'img/program-or-be-programmed.jpg', likes: 0 },
+    { name: 'Program or be Programmed', price: 8, pubdate: new Date('2013', '08', '01'), cover: 'img/program-or-be-programmed.jpg', likes: 0 },
+    { name: 'Program or be Programmed', price: 8, pubdate: new Date('2013', '08', '01'), cover: 'img/program-or-be-programmed.jpg', likes: 0 }];
+  $scope.plusOne = function(index) {
+    $scope.products[index].likes += 1;
+  };
 }]);
